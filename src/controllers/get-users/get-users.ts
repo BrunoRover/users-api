fd25c9b4c09ae4bf046153251ab64b18c89f5c64
Bruno <brunoplay055@gmail.com>
2024-11-28 17:06:39 -0300

    feat: add GetUsersController

---
+++ src/controllers/get-users/get-users.ts
@@ -0,0 +1,21 @@
+import { IGetUserController, IGetUsersRepository } from "./protocols";
+
+export class GetUserController implements IGetUserController {
+  constructor(private readonly getUsersRepository: IGetUsersRepository) {}
+  handle() {
+    try {
+      //validar req
+      // direcionar a chamada para o Repository
+      const users = await this.getUsersRepository.getUsers();
+      return {
+        statusCode: 200,
+        body: users,
+      };
+    } catch (error) {
+      return {
+        statusCode: 500,
+        body: "Something went wrong",
+      };
+    }
+  }
+}
